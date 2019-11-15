d621f066696a623c9ad27acc00d13d43ef770992
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-11-15 13:23:28 -0800

    skipped names for a moment, Reverse complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,18 +1,18 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    if self.current == len(self.storage):
-      self.current = 0
-      self.storage[self.current] = item
-      self.current += 1
-    else:
-      self.storage[self.current] = item
-      self.current += 1
+    def append(self, item):
+        if self.current == len(self.storage):
+            self.current = 0
+            self.storage[self.current] = item
+            self.current += 1
+        else:
+            self.storage[self.current] = item
+            self.current += 1
 
-  def get(self):
-    return [i for i in self.storage if i is not None]
+    def get(self):
+        return [i for i in self.storage if i is not None]
 
